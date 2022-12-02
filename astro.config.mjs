3902ac7dcedb6685184ca9381ee8e8e1cbbd7a19
murtuza <murtuza.creativity@gmail.com>
2022-12-02 21:05:58 +0530

    testing vite hmr timeout

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,12 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://murtuzaalisurti.github.io"
+    site: "https://murtuzaalisurti.github.io",
+    vite: {
+        server: {
+            hmr: {
+                timeout: 10000
+            }
+        }
+    }
 });
